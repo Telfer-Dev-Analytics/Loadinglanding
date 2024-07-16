915f3be7938363bbf0ed207c9c11bf27c83002c3
Reemaalwa <157423394+Reemaalwa@users.noreply.github.com>
2024-07-16 14:56:49 -0400

    fixed js config

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebas
 import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyA6pjR8bJ3JTEt-MporhAwEOPzd6VapiFQ",
     authDomain: "emailsignup-795ab.firebaseapp.com",
+    databaseURL: "https://emailsignup-795ab-default-rtdb.firebaseio.com",
     projectId: "emailsignup-795ab",
     storageBucket: "emailsignup-795ab.appspot.com",
     messagingSenderId: "601446445042",
     appId: "1:601446445042:web:a3baa91dddd0ce61ae056b",
     measurementId: "G-GGSSMDRG2K"
-};
-
+  };
+  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
